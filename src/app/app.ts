d0042ee1cb4040620fe2a916a6ed6e8437232ce0
neewt19 <newthonaraujo19@gmail.com>
2026-01-12 09:08:31 -0300

    feat: Define a sessão de conteúdo principal

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from "./components/header/header";
+import { MainContent } from "./components/main-content/main-content";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header],
+  imports: [RouterOutlet, Header, MainContent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
