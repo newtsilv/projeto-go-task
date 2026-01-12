78f925867bc7abb71219a1af96722d0cf0370031
neewt19 <newthonaraujo19@gmail.com>
2026-01-12 09:42:35 -0300

    feat: cria layout da sessão de boas-vindas

--- src/app/components/main-content/main-content.ts
+++ src/app/components/main-content/main-content.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { WelcomeSection } from "../welcome-section/welcome-section";
 
 @Component({
   selector: 'app-main-content',
-  imports: [],
+  imports: [WelcomeSection],
   templateUrl: './main-content.html',
   styleUrl: './main-content.css',
 })
