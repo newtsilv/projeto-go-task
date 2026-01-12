530980ad0f04fd5c9290548c0cbf0399ea724cb5
neewt19 <newthonaraujo19@gmail.com>
2026-01-12 11:23:37 -0300

    feat: cria o layout das colunas de tarefas e adiciona @utility scrollbar-hidden no styles.css

--- src/app/components/main-content/main-content.ts
+++ src/app/components/main-content/main-content.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { WelcomeSection } from "../welcome-section/welcome-section";
+import { TaskListSection } from "../task-list-section/task-list-section";
 
 @Component({
   selector: 'app-main-content',
-  imports: [WelcomeSection],
+  imports: [WelcomeSection, TaskListSection],
   templateUrl: './main-content.html',
   styleUrl: './main-content.css',
 })
