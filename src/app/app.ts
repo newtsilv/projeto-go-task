a9648f9b48b659f349b59276546c5473b65f57b4
neewt19 <newthonaraujo19@gmail.com>
2026-01-13 09:22:18 -0300

    feat: cria o layout do modal de criação e edição de tarefa

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,11 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from "./components/header/header";
 import { MainContent } from "./components/main-content/main-content";
+import { TaskFormModal } from "./components/task-form-modal/task-form-modal";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header, MainContent],
+  imports: [RouterOutlet, Header, MainContent, TaskFormModal],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
