c9e93318f7a58efe628e2dd0345658f41578d8d0
neewt19 <newthonaraujo19@gmail.com>
2026-01-13 11:24:50 -0300

    feat: cria o layout do modal de comentários da tarefa

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,10 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import { Header } from "./components/header/header";
 import { MainContent } from "./components/main-content/main-content";
 import { TaskFormModal } from "./components/task-form-modal/task-form-modal";
+import { TaskCommentsModal } from "./components/task-comments-modal/task-comments-modal";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header, MainContent, TaskFormModal],
+  imports: [RouterOutlet, Header, MainContent, TaskFormModal, TaskCommentsModal],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
