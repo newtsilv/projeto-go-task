0179d94bb6d0639d2d9ceaf5d3e0e5d3f689b47c
newtsilv <newthonaraujo19@gmail.com>
2026-01-25 18:34:45 -0300

    feat: renderiza as tarefas com AsyncPipe e @let e retira placeholder do card ao arrastar

--- src/app/components/task-list-section/task-list-section.ts
+++ src/app/components/task-list-section/task-list-section.ts
@@ -3,30 +3,18 @@ import { TaskCard } from '../task-card/task-card';
 import { TaskService } from '../../services/task.service';
 import { CdkDropList, CdkDrag, CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
 import { ITask } from '../../interfaces/task.interface';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-task-list-section',
-  imports: [TaskCard, CdkDropList, CdkDrag],
+  imports: [TaskCard, CdkDropList, CdkDrag, AsyncPipe],
   templateUrl: './task-list-section.html',
   styleUrl: './task-list-section.css',
 })
 export class TaskListSection {
-  todoTasks:ITask[] = []
-  doingTasks:ITask[] = []
-  doneTasks:ITask[] = []
 
-  private readonly _taskService = inject(TaskService);
-  ngOnInit() {
-    this._taskService.todoTasks.subscribe((tasks) => {
-      this.todoTasks = tasks;
-    });
-    this._taskService.doingTasks.subscribe((tasks) => {
-      this.doingTasks = tasks;
-    });
-    this._taskService.doneTasks.subscribe((tasks) => {
-      this.doneTasks = tasks;
-    });
-  }
+  public readonly _taskService = inject(TaskService);
+
 
   drop(event: CdkDragDrop<ITask[]>) {
     if (event.previousContainer === event.container) {
