7a29a20019e0272ca2ac21d03b4101bf28717b32
newtsilv <newthonaraujo19@gmail.com>
2026-01-25 18:17:12 -0300

    feat: configura o Drag and Drop nas colunas de status da tarefa

--- src/app/components/task-list-section/task-list-section.ts
+++ src/app/components/task-list-section/task-list-section.ts
@@ -1,18 +1,47 @@
 import { Component, inject } from '@angular/core';
 import { TaskCard } from '../task-card/task-card';
 import { TaskService } from '../../services/task.service';
+import { CdkDropList, CdkDrag, CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
+import { ITask } from '../../interfaces/task.interface';
 
 @Component({
   selector: 'app-task-list-section',
-  imports: [TaskCard],
+  imports: [TaskCard, CdkDropList, CdkDrag],
   templateUrl: './task-list-section.html',
   styleUrl: './task-list-section.css',
 })
 export class TaskListSection {
+  todoTasks:ITask[] = []
+  doingTasks:ITask[] = []
+  doneTasks:ITask[] = []
+
   private readonly _taskService = inject(TaskService);
   ngOnInit() {
     this._taskService.todoTasks.subscribe((tasks) => {
-      console.log('Tarefas a fazer:', tasks);
+      this.todoTasks = tasks;
     });
+    this._taskService.doingTasks.subscribe((tasks) => {
+      this.doingTasks = tasks;
+    });
+    this._taskService.doneTasks.subscribe((tasks) => {
+      this.doneTasks = tasks;
+    });
+  }
+
+  drop(event: CdkDragDrop<ITask[]>) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex
+      );
+    } else {
+      transferArrayItem(
+        event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex
+      )
+    }
   }
 }
