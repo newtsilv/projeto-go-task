0dfb0b91f8519e771cd66f00b24fe17c9ec3b159
neewt19 <newthonaraujo19@gmail.com>
2026-01-27 11:39:16 -0300

    feat: limitando o nome e a descrição da tarefa com pipe Slice

--- src/app/components/task-card/task-card.ts
+++ src/app/components/task-card/task-card.ts
@@ -2,10 +2,11 @@ import { Component, inject, Input } from '@angular/core';
 import { modalControllerService } from '../../services/modal-controller.service';
 import { ITask } from '../../interfaces/task.interface';
 import { TaskService } from '../../services/task.service';
+import { SlicePipe } from '@angular/common';
 
 @Component({
   selector: 'app-task-card',
-  imports: [],
+  imports: [SlicePipe],
   templateUrl: './task-card.html',
   styleUrl: './task-card.css',
 })
